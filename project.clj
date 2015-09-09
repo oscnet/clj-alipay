3b4fc6898bb608338edd14eaadfac64e9f1c7062
oscar <oscnet@163.com>
2015-09-09 20:30:15 +0800

    bumped up version

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-alipay "0.1.1"
+(defproject clj-alipay "0.1.2"
   :description "支付宝即时到账交易接口(create_direct_pay_by_user)"
   :url "http://www.zjpjhx.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-http "1.0.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]
                  [hiccup "1.0.5"]
                  [crypto-random "1.2.0"]
-                 [lib-noir "0.9.5"]]
+                 [lib-noir "0.9.9"]]
   :profiles {:test {:dependencies [[ring/ring-mock "0.2.0"]
                                    [clj-http-fake "1.0.1"]]}})
